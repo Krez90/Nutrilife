1bffac813d67bf5b144baad6ef0a02418e54a512
TA-A <thecleaurelie.augustin@gmail.com>
2019-02-07 15:40:08 +0100

    front

--- search.js
+++ search.js
@@ -15,34 +15,34 @@ var options = {
 					success: function (response) {//response is value returned from php (for your example it's "bye bye"
 						var responseJson = $.parseJSON(response);
 						$(".reponse").css({
-							backgroundColor : "rgba(151, 231, 226, 0.671)",
+							backgroundColor : "#e6fbfe",
 							marginBottom:"20px",
 							borderRadius:"20px",
-							paddingTop: "5%",
-							paddingRight: "3%",
-							paddingLeft: "3%",
-							paddingBottom: "5%"});
-						// $("#prescription").css("text-align","left");
+							paddingTop: "10%",
+							paddingRight: "15%",
+							paddingLeft: "15%",
+							paddingBottom: "10%",
+							display: "block"});
 						$("#nom").text(responseJson.denomination);
-						$("#pres_titre").text("Prescrition :");
 						$("#prescription").html(responseJson.indicationsTherapeutiques).css("text-align","left");
-						$("#prix_titre").text("Prix :");
-						$("#prix").text(responseJson.presentations[0].prix == null ? "libre" : responseJson.presentations[0].prix);
+						$("#prix_titre").text("Prix en euros :");
+						$("#prix").text(responseJson.presentations[0].prix == null ? "libre." : responseJson.presentations[0].prix);
 						$("#taux_titre").text("Taux de remboursement :");
-						$("#taux").text(responseJson.presentations[0].tauxRemboursement.length == 0 ? "Non remboursé" : responseJson.presentations[0].tauxRemboursement);
+						$("#taux").text(responseJson.presentations[0].tauxRemboursement.length == 0 ? "Non remboursé." : responseJson.presentations[0].tauxRemboursement);
 						$("#IR_titre").text("Indication de remboursement :");
-						$("#indicremboursement").html(responseJson.presentations[0].indicationsRemboursement.length == 0 ? "Non renseigné" : responseJson.presentations[0].indicationsRemboursement);
+						$("#indicremboursement").html(responseJson.presentations[0].indicationsRemboursement.length == 0 ? "Non renseigné." : responseJson.presentations[0].indicationsRemboursement);
 						$("#gene_titre").text("Générique(s) :");
 						if (responseJson.infosGenerique) {
 							var content = "";	
 							for (var i = 0; i < responseJson.infosGenerique.autresMedicamentsGroupe.length; i++) {
-								content += responseJson.infosGenerique.autresMedicamentsGroupe[i].denomination + "<br><br>";
+								content += responseJson.infosGenerique.autresMedicamentsGroupe[i].denomination + "<br>";
 							}
 							// console.log(content);
-							$("#generique").html(content);
+							// $("#generique").html(content);
+							$("#generique").html(content.toLowerCase());
 						}else{
 							// console.log(content);
-							$("#generique").html(responseJson.infosGenerique == null ? "Aucun" : responseJson.infosGenerique);
+							$("#generique").html(responseJson.infosGenerique == null ? "Aucun." : responseJson.infosGenerique);
 						}
 					}
 			 });
@@ -50,6 +50,7 @@ var options = {
 			}
 		},
 
+// AJAX SETTINGS //
 	ajaxSettings: {
 		dataType: "json",
 		method: "POST",
@@ -68,3 +69,12 @@ var options = {
 };
 
 $("#provider-json").easyAutocomplete(options);
+
+
+// animation navbar//
+$( ".button-group > div" ).click(function() {
+	$('.button-group > div.active').not(this).removeClass('active');
+	$( this ).toggleClass( "active" );
+  });
+
+
