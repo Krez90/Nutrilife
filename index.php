0cadccaac6cdf591d74ff73eb158f585fe4bff7c
Neithy88 <alexandra.moullet@gmail.com>
2019-02-07 15:42:07 +0100

    merge with branche

--- index.php
+++ index.php
@@ -78,15 +78,11 @@
 
             <section class="hero text-center" id ="geolocalisation">
             <div class="container-sm">
-            <i class ="fas fa-map-marked-alt markedpage" id="logopage"></i>
-
-                
+                <i class ="fas fa-map-marked-alt markedpage" id="logopage"></i>                   
                 <h3>Localiser les pharmacies autour de vous !</h3>
-                <button id="loc-bt">Géolocalisation</button>
-               
-                
+                <button id="loc-bt">Géolocalisation</button>  
                 <div id="map" >
-                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d85999.70351495885!2d6.829584885290534!3d47.65546238480941!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1549457707571" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe> 
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d85999.70351495885!2d6.829584885290534!3d47.65546238480941!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1549457707571" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe> 
                 </div>
             
                
@@ -127,10 +123,7 @@
 			
             <div class="container">
                 <div class="site-footer-inner has-top-divider">
-                    <div class="brand footer-brand">
-                    </div>
-                    <ul class="footer-social-links list-reset">
-                    </ul>
+                
                     <div class="footer-copyright">&copy; 2019 Pharma net, all rights reserved</div>
                 </div>
             </div>
