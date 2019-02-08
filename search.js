5108c3dd460583233e19a9eafde1b0f7530e07fa
Neithy88 <alexandra.moullet@gmail.com>
2019-02-08 08:59:42 +0100

    commit 2cnd version

--- search.js
+++ search.js
@@ -16,33 +16,33 @@ var options = {
 						var responseJson = $.parseJSON(response);
 						$(".reponse").css({
 							backgroundColor : "#e6fbfe",
-							marginBottom:"20px",
+							marginBottom:"15px",
 							borderRadius:"20px",
-							paddingTop: "10%",
-							paddingRight: "15%",
-							paddingLeft: "15%",
-							paddingBottom: "10%",
-							display: "block"});
+							paddingTop: "5%",
+							paddingRight: "5%",
+							paddingLeft: "5%",
+							paddingBottom: "5%",
+							display: "block",});
 						$("#nom").text(responseJson.denomination);
 						$("#prescription").html(responseJson.indicationsTherapeutiques).css("text-align","left");
 						$("#prix_titre").text("Prix en euros :");
-						$("#prix").text(responseJson.presentations[0].prix == null ? "libre." : responseJson.presentations[0].prix);
+						$("#prix").text(responseJson.presentations[0].prix == null ? "libre" : responseJson.presentations[0].prix);
 						$("#taux_titre").text("Taux de remboursement :");
-						$("#taux").text(responseJson.presentations[0].tauxRemboursement.length == 0 ? "Non remboursé." : responseJson.presentations[0].tauxRemboursement);
-						$("#IR_titre").text("Indication de remboursement :");
-						$("#indicremboursement").html(responseJson.presentations[0].indicationsRemboursement.length == 0 ? "Non renseigné." : responseJson.presentations[0].indicationsRemboursement);
+						$("#taux").text(responseJson.presentations[0].tauxRemboursement.length == 0 ? "N/R" : responseJson.presentations[0].tauxRemboursement);
+						// $("#IR_titre").text("Indication de remboursement :");
+						// $("#indicremboursement").html(responseJson.presentations[0].indicationsRemboursement.length == 0 ? "Inconnue" : responseJson.presentations[0].indicationsRemboursement);
 						$("#gene_titre").text("Générique(s) :");
 						if (responseJson.infosGenerique) {
 							var content = "";	
 							for (var i = 0; i < responseJson.infosGenerique.autresMedicamentsGroupe.length; i++) {
-								content += responseJson.infosGenerique.autresMedicamentsGroupe[i].denomination + "<br>";
+								content += responseJson.infosGenerique.autresMedicamentsGroupe[i].denomination+ "<br>";
 							}
 							// console.log(content);
 							// $("#generique").html(content);
 							$("#generique").html(content.toLowerCase());
 						}else{
 							// console.log(content);
-							$("#generique").html(responseJson.infosGenerique == null ? "Aucun." : responseJson.infosGenerique);
+							$("#generique").html(responseJson.infosGenerique == null ? "Aucun" : responseJson.infosGenerique);
 						}
 					}
 			 });
